8187e031ce2b402f4bf75148815876fab28a90f8
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-10 17:27:46 -0500

    not sure

--- api/server.js
+++ api/server.js
@@ -4,10 +4,7 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/api/users', (req , res) =>{
-res.status(200).json({"name":"fatimah", "bio":"developer"
-})
-})
+
 server.get('/', (req,res)=>{
     res.end({"name":"fatimah", "bio":"developer"})
 });
