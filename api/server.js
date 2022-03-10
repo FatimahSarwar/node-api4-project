8c366bfd0c8b6d027f9cc9a13d2bd38844b523eb
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-10 17:00:40 -0500

    hopeful

--- api/server.js
+++ api/server.js
@@ -4,7 +4,10 @@ const User = require('./users/user-model');
 
 server.use(express.json());
 
-server.get('/api/users', (req,res)=>{
- res.status(200).json(User.users)
+server.get('/api/users', (req , res) =>{
+    User.find()
+    .then(user =>{
+        res.status(200).json(user)
+    })
 })
 module.exports = server;
--- api/users/user-model.js
+++ api/users/user-model.js
@@ -1,10 +1,21 @@
-const initializeUsers =  [
-    {  name: 'Fatimah Sarwar', bio: 'developer' },
-    {name: 'Arman Mridha', bio: 'engineer' }
-  ]
+const { nanoid } = require('nanoid')
+
+function getId() {
+  return nanoid().slice(0, 5)
+}
+
+const initializeUsers = () => ([
+  { id: getId(), name: 'Ed Carter', bio: 'hero' },
+  { id: getId(), name: 'Mary Edwards', bio: 'super hero' },
+])
 
 
-  let users = initializeUsers;
+let users = initializeUsers()
 
 
-module.exports = {users};
+const find = () => {
+
+  return Promise.resolve(users)
+}
+
+module.exports = {find};
