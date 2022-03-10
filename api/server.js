bd5a4ff99116b8592f04ad297e92b5fed246f6e1
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-10 17:19:42 -0500

    maybe

--- api/server.js
+++ api/server.js
@@ -8,4 +8,8 @@ server.get('/api/users', (req , res) =>{
 res.status(200).json({name:"fatimah", bio:"developer"
 })
 })
+server.get('/', (req,res)=>{
+    res.end('<h1>Hello World</h1>')
+})
+
 module.exports = server;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const server = require('./api/server');
 require('dotenv').config();
 const PORT = process.env.PORT || 9000;
 
+
 // START YOUR SERVER HERE
 server.listen(PORT, ()=>{
     console.log("server is running" , PORT, process.env.USER)
