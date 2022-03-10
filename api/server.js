663e9e1df4734718825fa38b6d040113272268bc
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-10 17:03:49 -0500

    hmm

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ server.use(express.json());
 server.get('/api/users', (req , res) =>{
     User.find()
     .then(user =>{
-        res.status(200).json(user)
+        res.json(user)
     })
 })
 module.exports = server;
